756a34b24cd3d14aded380e6ebe55a564105ed0c
Vinod Kurup <vinod@kurup.com>
2011-10-21 13:43:51 -0400

    Done for now
    Sorry for not documenting that step by step like I meant to...

--- amion_schedule.rb
+++ amion_schedule.rb
@@ -1,4 +1,118 @@
-class AmionSchedule
-  def initialize
+require 'csv'
+require 'open-uri'
+require 'date'
+
+def get_one_day_from_amion(password)
+  csv = get_all_csv_data(password, Date.today)
+  s = make_schedule(csv)
+  ds = one_day_schedule(s, Date.today)
+end
+
+private
+
+def get_one_file(password, month, year)
+  url = URI.escape("http://www.amion.com/cgi-bin/ocs?Lo=#{password}&Rpt=619&Month=#{month}&Year=#{year}")
+  data = open(url).read
+  if data =~ /NOFI=No file/
+    raise "Invalid AMION password."
+  else
+    data
   end
 end
+
+def get_all_csv_data(password, date)
+  # Choose 2 dates which will cover all possible stretch lengths.
+  # A stretch is never more than 10 days long, so 30 days will
+  # definitely cover it.
+  dates = [date - 15, date + 15]
+  schedules_to_get = dates.map { |d| { :month => d.month, :year => d.year} }.uniq
+
+  data = []
+  schedules_to_get.each do |sched|
+    # parse the CSV and strip the first 6 lines (HEADER)
+    csv = CSV.parse(get_one_file(password, sched[:month], sched[:year])).slice(6..-1)
+    data += csv
+  end
+  return data
+end
+
+def parse_date(line)
+  Date.strptime(line[6],'%m-%e-%y')
+end
+
+def parse_name(line)
+  line[0]
+end
+
+def parse_long_shift(line)
+  line[3]
+end
+
+def parse_shift(line)
+  ls = parse_long_shift(line)
+  return "Flex" if ls =~ /^Hospitalist 7/
+  return "12-9" if ls =~ /Day Admit/
+  return "Day Shift" if ls =~ /^Hospitalist [1-6]/
+  return ls.split[0]
+end
+
+def parse_service(line)
+  ls = parse_long_shift(line)
+  service = /\((5.[123])\)/.match(ls)
+  service[1] if service
+end
+
+def make_schedule(csv_data)
+  schedule = {}
+
+  csv_data.each do |line|
+    # skip unwanted shifts
+    next if parse_long_shift(line) =~ /^Teaching|^Administrator|^Jeopardy|^NP-PA|^Ortho|Palliative|^9050 Morning/
+
+    # date will be our key
+    date = parse_date(line)
+
+    name = parse_name(line)
+    shift = parse_shift(line)
+    service = parse_service(line)
+
+    # create or append to the date's entry in our schedule
+    schedule[date] ||= []
+    schedule[date] << { name: name, shift: shift, service: service}
+  end
+
+  return schedule
+end
+
+def one_day_schedule(schedule, date)
+  day_schedule = schedule[date]
+
+  day_schedule.map! do |hosp|
+    if hosp[:shift] == "Day Shift"
+      # Calculate which day of the stretch they are
+      hosp_day = 1
+      # Check if they worked yesterday. If so, increment hosp_day
+      day = date - 1
+      while schedule[day].index{ |past| past[:name] == hosp[:name] and past[:shift] == "Day Shift"} 
+        hosp_day += 1
+        day -= 1
+      end
+      
+      hosp_stretch = hosp_day
+      day = date + 1
+      while schedule[day].index{ |future| future[:name] == hosp[:name] and future[:shift] == "Day Shift"} 
+        hosp_stretch += 1
+        day += 1
+      end
+      
+      hosp[:day] = hosp_day
+      hosp[:stretch] = hosp_stretch
+    else
+      hosp[:day] = 0
+      hosp[:stretch] = 0
+    end
+    hosp
+  end
+  day_schedule
+end
+
--- amion_schedule_test.rb
+++ amion_schedule_test.rb
@@ -1,10 +1,53 @@
 require 'test/unit'
-require 'pathname'
 require File.expand_path(File.join(File.dirname(__FILE__)), 'amion_schedule')
 
 class TestAmionSchedule < Test::Unit::TestCase
-  def test_initialize
-    as = AmionSchedule.new
-    assert_not_nil(as)
+  # def test_bad_password_returns_error
+  #   exception = assert_raise(RuntimeError) {
+  #     get_one_file("badpwd")
+  #   }
+  #   assert_equal "Invalid AMION password.", exception.message
+  # end
+
+  # def test_good_password_does_not_return_error
+  #   as = get_one_file("DRH Hospitalist")
+  #   assert_not_equal "Invalid password.", as
+  # end
+
+  def setup
+    @kurup = ["Kurup", "248", "13", "Hospitalist 2       (5-1)   7a-7p", "278", "35", "10-31-11", "0700", "1900"]
+    @flex = ["Wozniak", "433", "325", "Hospitalist 7          7a-7p", "283", "40", "10-31-11", "0700", "1900"]
+    @day_admit = ["Johnson", "236", "1", "9050 Day Admit Shift 12p-9p", "289", "45", "10-31-11", "1200", "2100"]
+  end
+
+  def test_date_parse_is_correct
+    assert_equal Date.parse('2011-10-31'), parse_date(@kurup)
+  end
+
+  def test_name_parse_is_correct
+    assert_equal "Kurup", parse_name(@kurup)
+  end
+
+  def test_shift_parse_is_correct
+    assert_equal "Day Shift", parse_shift(@kurup)
+    assert_equal "Flex", parse_shift(@flex)
+    assert_equal "12-9", parse_shift(@day_admit)
+  end
+
+  def test_service_parse_is_correct
+    assert_equal "5-1", parse_service(@kurup)
+    assert_equal nil, parse_service(@flex)
+  end
+
+  def test_one_day_schedule_is_correct
+    s = make_schedule(CSV.read("test.csv").slice(6..-1))
+    date = Date.parse("2011/10/14")
+    ds = one_day_schedule(s, date)
+    kurup = ds.index{ |h| h[:name] == "Kurup"}
+
+    assert_equal 1, ds[kurup][:day], "Day is correct"
+    assert_equal 5, ds[kurup][:stretch], "Stretch is correct"
+    assert_equal "Day Shift", ds[kurup][:shift], "Shift is correct"
+    assert_equal "5-2", ds[kurup][:service], "service is correct"
   end
 end
